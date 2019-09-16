2ec3492823e06331cd430c55a24beddaf4b37ad7
German Tellez <gertellezv@gmail.com>
2019-09-16 18:22:49 -0500

    Botones de lenguajes terminados

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function ocultar()
+function mostrarTodos()
 {
     if (document.getElementById){
-        elemento = document.getElementById('recocido');
+        recocido = document.getElementById('recocido');
+        wavefront = document.getElementById('wavefront');
         hillClimbing = document.getElementById('hillClimbing');
         yartpApi= document.getElementById('yartpApi');
         controlRemoto= document.getElementById('controlRemoto');
@@ -9,11 +10,44 @@ function ocultar()
         inventario = document.getElementById('inventario');
 
 
-        elemento.style.display = (elemento.style.display == 'none') ? 'block' : 'none';
-        hillClimbing.style.display = (hillClimbing.style.display == 'none') ? 'block' : 'none';
-        yartpApi.style.display = (yartpApi.style.display == 'none') ? 'block' : 'none';
-        controlRemoto.style.display = (controlRemoto.style.display == 'none') ? 'block' : 'none';
-        contadorPalabras.style.display = (contadorPalabras.style.display == 'none') ? 'block' : 'none';
-        inventario.style.display = (inventario.style.display == 'none') ? 'block' : 'none';
+        recocido.style.display = 'block';
+        wavefront.style.display = 'block';
+        hillClimbing.style.display = 'block';
+        yartpApi.style.display = 'block';
+        controlRemoto.style.display = 'block';
+        contadorPalabras.style.display = 'block';
+        inventario.style.display = 'block';
+
+        // SI QUIERO QUE SE MUESTRE U OCULTE AL PRESIONAR CLIC ACA DEBAJO ESTA EL CODIGO PARA ELLO
+
+        // elemento.style.display = (elemento.style.display == 'none') ? 'block' : 'none';
+        // hillClimbing.style.display = (hillClimbing.style.display == 'none') ? 'block' : 'none';
+        // yartpApi.style.display = (yartpApi.style.display == 'none') ? 'block' : 'none';
+        // controlRemoto.style.display = (controlRemoto.style.display == 'none') ? 'block' : 'none';
+        // contadorPalabras.style.display = (contadorPalabras.style.display == 'none') ? 'block' : 'none';
+        // inventario.style.display = (inventario.style.display == 'none') ? 'block' : 'none';
+    }
+}
+
+function mostrarPython()
+{
+    if (document.getElementById){
+        recocido = document.getElementById('recocido');
+        wavefront = document.getElementById('wavefront');
+        hillClimbing = document.getElementById('hillClimbing');
+        yartpApi= document.getElementById('yartpApi');
+        controlRemoto= document.getElementById('controlRemoto');
+        contadorPalabras = document.getElementById('contadorPalabras');
+        inventario = document.getElementById('inventario');
+
+        //mostrar
+        recocido.style.display = 'block';
+        wavefront.style.display = 'block';
+        hillClimbing.style.display = 'block';
+        yartpApi.style.display = 'block';
+        inventario.style.display = 'block';
+        // ocultar
+        controlRemoto.style.display = 'none';
+        contadorPalabras.style.display = 'none';
     }
 }
