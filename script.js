d6e57580407e27a23074d99b32c45763bfd5fb91
German Tellez <gertellezv@gmail.com>
2019-09-16 17:44:51 -0500

    Boton mostrar/ocultar todo terminado

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ function ocultar()
 {
     if (document.getElementById){
         elemento = document.getElementById('recocido');
+        hillClimbing = document.getElementById('hillClimbing');
+        yartpApi= document.getElementById('yartpApi');
+        controlRemoto= document.getElementById('controlRemoto');
+        contadorPalabras = document.getElementById('contadorPalabras');
+        inventario = document.getElementById('inventario');
+
 
         elemento.style.display = (elemento.style.display == 'none') ? 'block' : 'none';
-        
+        hillClimbing.style.display = (hillClimbing.style.display == 'none') ? 'block' : 'none';
+        yartpApi.style.display = (yartpApi.style.display == 'none') ? 'block' : 'none';
+        controlRemoto.style.display = (controlRemoto.style.display == 'none') ? 'block' : 'none';
+        contadorPalabras.style.display = (contadorPalabras.style.display == 'none') ? 'block' : 'none';
+        inventario.style.display = (inventario.style.display == 'none') ? 'block' : 'none';
     }
 }
